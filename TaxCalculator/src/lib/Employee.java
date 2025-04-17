50b5562f33e4d6956ef5c0cdb2dc8f88985386f5
tikature <detikaa10@gmail.com>
2025-04-17 13:07:24 +0700

    Memperbaiki metode getAnnualIncomeTax untuk mengurangi ketergantungan (coupling) dengan TaxFunction dan menghindari akses langsung ke field spouse dan children.

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -97,16 +97,20 @@ public class Employee {
 	}
 	
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		int monthWorkingInYear = LocalDate.now().getYear() == joinDate.getYear()
+			? LocalDate.now().getMonthValue() - joinDate.getMonthValue()
+			: 12;
+	
+		boolean isMarried = spouse != null;
+		int numberOfChildren = children.size();
+	
+		return TaxFunction.calculateTax(
+			monthlySalary,
+			otherMonthlyIncome,
+			monthWorkingInYear,
+			annualDeductible,
+			isMarried,
+			numberOfChildren
+		);
 	}
 }
